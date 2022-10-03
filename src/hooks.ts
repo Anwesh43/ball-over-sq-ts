dc622e61ce17dc8d6f14dca25184cee635ad2d86
Anwesh43 <anweshthecool0@gmail.com>
2022-10-03 16:24:40 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,8 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 const delay : number = 20 
 const scGap : number = 0.01
+const sizeFactor : number = 12
 
 export const useAnimatedScale = () => {
     const [scale, setScale] = useState(0)
@@ -35,12 +36,62 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     }, [])
     return {
         w, 
         h
+    }
+}
+
+const sinify = (scale : number) : number => Math.sin(scale * Math.PI)
+
+export const useStyle = (w : number, h : number, scale : number) => {
+
+    const position = 'absolute'
+    const sf : number = sinify(scale)
+    const size = Math.min(w, h) / sizeFactor 
+    const background = 'indigo'
+    return {
+        parentStyle() : CSSProperties {
+            const top = `${w / 2}px`
+            const left = `${h / 2}px`
+            return {
+                position, 
+                top, 
+                left 
+            }
+        },
+        ballStyle() {
+            const top = `${-size - (h / 2) * sf}px`
+            const left = `${-size / 2}px`
+            const width = `${size}px`
+            const height = `${size}px`
+            return {
+                position, 
+                top, 
+                left, 
+                width, 
+                height, 
+                borderRadius: '50%',
+                background 
+            }
+        },
+        squareStyle() {
+            const left = `${-size / 2}px`
+            const top = `${0}px`
+            const width = `${size}px`
+            const height = `${size}px`
+            return {
+                position, 
+                left, 
+                top, 
+                background, 
+                width, 
+                height 
+            }
+        }
     }
 }
